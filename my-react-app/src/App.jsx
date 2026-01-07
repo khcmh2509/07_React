6ed147620649f7f1b52d7e63a53a51f624de5ce8
khcmh2509 <khcmh2509@gmail.com>
2026-01-07 15:12:31 +0900

    상태 끌어올리기,내리꽂기, JSX 예제

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -4,6 +4,9 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import Exam1 from './components/Exam1'
 import Exam2 from './components/Exam2'
+import Exam3 from './components/Exam3'
+import Exam4 from './components/Exam4'
+import Exam5 from './components/Exam5'
 
 function App() {
   // 상태 (state)
@@ -17,13 +20,13 @@ function App() {
     <>
       {/* jsx 주석 */}
       {/* <h1>안녕하세요!</h1> */}
-      <button onClick={() => setShowExam(!showExam)}>클릭</button>
-      {showExam && <Exam2 mihyun="hello" test="world" />}
+      {/* <button onClick={() => setShowExam(!showExam)}>클릭</button>
+      {showExam && <Exam2 mihyun="hello" test="world" />} */}
       {/* 조건부 렌더링 : 조건에 따라 렌더링되는 방법 
         && 앞에있는 showExam이 true 면 Exam1 렌더링됨
                           false 면 Exam1 렌더링 되지 않음
       */}
-      
+      <Exam5 />
     </>
   )
 }
